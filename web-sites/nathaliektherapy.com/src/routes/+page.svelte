2e751a413f53cffe20af604d9e6e87eb2b1474f2
aryadaroui <adaroui@uci.edu>
2026-01-18 20:06:27 -0800

    more robust

--- web-sites/nathaliektherapy.com/src/routes/+page.svelte
+++ web-sites/nathaliektherapy.com/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to nathaliektherapy.com</h1>
+<p>Please check back soon for more information.</p>
+
+
+<style>
+	h1 {
+		color: #2c3e50;
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+		text-align: center;
+		margin-top: 50px;
+	}
+
+	p {
+		color: #34495e;
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+		text-align: center;
+		font-size: 18px;
+	}
+
+</style>
